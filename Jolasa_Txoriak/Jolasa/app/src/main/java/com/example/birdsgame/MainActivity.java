25e6639f29aaa2c8fa426915a8d1fd3a2443163d
Karmele Elorza <elorza.karmele@uni.eus>
2022-11-29 13:27:48 +0100

    angryBirds

--- Jolasa_Txoriak/Jolasa/app/src/main/java/com/example/birdsgame/MainActivity.java
+++ Jolasa_Txoriak/Jolasa/app/src/main/java/com/example/birdsgame/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.birdsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,10 +13,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imgFondoa);
-        for(int i = 0; i < 500; i++){
-            imageView.setScrollX(i);
-        }
+        findViewById(R.id.btnPlay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
     }
 }
+
+//https://www.youtube.com/watch?v=EBJDo9a1q-o  0:44:00
